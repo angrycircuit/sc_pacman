80d81af8981550a8c3589c880e631eb95a7347f5
honeyrocketeeter <59887792+honeyrocketeeter@users.noreply.github.com>
2020-01-17 02:32:21 -0500

    new file sc_pacman.c
    Creating a new file as a placeholder to start the project.

---
+++ sc_pacman.c
@@ -0,0 +1,17 @@
+/***************************************/
+/* SC Pacman Game                      */
+/*                                     */
+/* This is a fun project for IPC144NRR */
+/* with input from various member of   */
+/* of the class.                       */
+/*                                     */
+/* Programming is fun!                 */
+/*                                     */
+/***************************************/
+
+#include <stdio.h>
+
+int main() {
+  printf("SC Pac-Man!");
+  return 0;
+}
