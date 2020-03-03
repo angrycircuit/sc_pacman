25c7f2554e83bd43479572fb02d6baa15044a212
MadLeon <60018884+MadLeon@users.noreply.github.com>
2020-03-03 00:20:56 -0500

    Add files via upload

--- Include/declaration.h
+++ Include/declaration.h
@@ -1,28 +1,40 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
+// the pacman's moving distance in each frame
 #define VELOCITY 1
-//Screen dimension constants
+
+// screen dimension constants
 #define SCREEN_WIDTH 580
 #define SCREEN_HEIGHT 600
+
+// the size of rendering
 #define PACMAN_WIDTH 40
 #define PACMAN_HEIGHT 40
+
+// the inner boundaries of the map
 #define MAP_LEFT_INNER 20
 #define MAP_RIGHT_INNER 540
 #define MAP_UP_INNER 20
 #define MAP_DOWN_INNER 600
 
+// the speed of animation change
 #define PACMAN_ANIMATION_FRAMES 4
 
 struct ObjectMoveInfo
 {
-	int posx;
+	// position on x & y axis
+	int posx;	
 	int posy;
+	// speed on each axis
 	int velx;
 	int vely;
+	// the size of hitbox
 	SDL_Rect hitBox;
 };
 
+// import functions declared in "functions.c"
+// so it can be used in "pacman.c" instantly
 extern void free(SDL_Texture*);
 extern void handleEvent(struct ObjectMoveInfo* object, SDL_Event* e);
 extern void move(struct ObjectMoveInfo* object, SDL_Rect targetHitBox, int objWidth, int objHeight);
