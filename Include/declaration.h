14807d3fa66043def1b5719454b0cf5275b8ab5c
MadLeon <60018884+MadLeon@users.noreply.github.com>
2020-03-02 22:07:08 -0500

    Add files via upload

---
+++ Include/declaration.h
@@ -0,0 +1,30 @@
+#include <SDL.h>
+#include <SDL_image.h>
+
+#define VELOCITY 1
+//Screen dimension constants
+#define SCREEN_WIDTH 580
+#define SCREEN_HEIGHT 600
+#define PACMAN_WIDTH 40
+#define PACMAN_HEIGHT 40
+#define MAP_LEFT_INNER 20
+#define MAP_RIGHT_INNER 540
+#define MAP_UP_INNER 20
+#define MAP_DOWN_INNER 600
+
+#define PACMAN_ANIMATION_FRAMES 4
+
+struct ObjectMoveInfo
+{
+	int posx;
+	int posy;
+	int velx;
+	int vely;
+	SDL_Rect hitBox;
+};
+
+extern void free(SDL_Texture*);
+extern void handleEvent(struct ObjectMoveInfo* object, SDL_Event* e);
+extern void move(struct ObjectMoveInfo* object, SDL_Rect targetHitBox, int objWidth, int objHeight);
+extern int checkCollision(SDL_Rect a, SDL_Rect b);
+extern SDL_Texture* loadFromFile(SDL_Renderer* renderer, char* address[]);
